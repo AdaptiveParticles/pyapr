24e5c352d905e4719cc89ac17c6ea7c49989845f
joeljonsson <joeljo@student.chalmers.se>
2019-07-03 15:23:38 +0200

    simplified setup script

--- setup.py
+++ setup.py
@@ -1,42 +1,5 @@
-import sys, os
 from setuptools import setup, find_packages
-import subprocess
 from cmake_setuptools import *
-
-
-def check_submodules():
-    """ verify that the submodules are checked out and clean
-        use `git submodule update --init`; on failure
-    """
-    if not os.path.exists('.git'):
-        return
-    with open('.gitmodules') as f:
-        for l in f:
-            if 'path' in l:
-                p = l.split('=')[-1].strip()
-                if not os.path.exists(p):
-                    raise ValueError('Submodule %s missing' % p) #call git clone if this occurs???
-
-    proc = subprocess.Popen(['git', 'submodule', 'status'],
-                            stdout=subprocess.PIPE)
-    status, _ = proc.communicate()
-    status = status.decode("ascii", "replace")
-    for line in status.splitlines():
-        if line.startswith('-') or line.startswith('+'):
-            raise ValueError('Submodule not clean: %s' % line)
-
-
-class git_clone_external(CMakeBuildExt):
-    def run(self):
-
-        check_submodules()
-
-        #not sure if this works as intended
-        pyv = '{}.{}'.format(sys.version_info[0], sys.version_info[1])
-        os.environ['CMAKE_COMMON_VARIABLES'] = '-DPYBIND11_PYTHON_VERSION={}'.format(pyv)
-
-        CMakeBuildExt.run(self)
-
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -46,14 +9,14 @@ setup(
     version='0.1.1',
     ext_modules=[CMakeExtension('_pyaprwrapper')],
     cmdclass={
-        'build_ext': git_clone_external,
+        'build_ext': CMakeBuildExt,
     },
     setup_requires=['cmake-setuptools'],
     packages=find_packages(),
     install_requires=[
         'numpy',
         'torch',
-        'torchvision'
+        'torchvision',
     ],
     description='Python wrappers for LibAPR',
     long_description=long_description,
