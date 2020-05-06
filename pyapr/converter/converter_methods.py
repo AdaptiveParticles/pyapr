7bdf4db73475ff068080fcff81549d05bb0383cb
joeljonsson <jonsson@mpi-cbg.de>
2020-05-06 15:40:37 +0200

    modified / fixed get_apr_interactive function

--- pyapr/converter/converter_methods.py
+++ pyapr/converter/converter_methods.py
@@ -1,32 +1,38 @@
 import pyapr
 
 
-def get_apr_interactive(image, dtype='float', rel_error=0.1, gradient_smoothing=2, verbose=True):
+def get_apr_interactive(image, dtype='short', rel_error=0.1, gradient_smoothing=2, verbose=True, params=None):
+
     # Initialize objects
     io_int = pyapr.filegui.InteractiveIO()
     apr = pyapr.APR()
-    par = pyapr.APRParameters()
-    if dtype == 'float':
+    if params is None:
+        par = pyapr.APRParameters()
+
+        # Set some parameters
+        par.auto_parameters = False
+        par.rel_error = rel_error
+        par.gradient_smoothing = gradient_smoothing
+    else:
+        par = params
+
+    if dtype in ('float', 'float32'):
         parts = pyapr.FloatParticles()
         converter = pyapr.converter.FloatConverter()
-    elif dtype == 'short':
+    elif dtype in ('short', 'uint16'):
         parts = pyapr.ShortParticles()
         converter = pyapr.converter.ShortConverter()
-    elif dtype == 'byte':
-        parts = pyapr.ByteParticles()
-        converter = pyapr.converter.ByteConverter()
+    # elif dtype in {'byte', 'uint8'}:  # currently not working
+    #     parts = pyapr.ByteParticles()
+    #     converter = pyapr.converter.ByteConverter()
     else:
-        raise Exception("get_apr_interactive dtype must be 'float', 'short' or 'byte' ")
+        errstr = 'get_apr_interactive argument dtype must be one of (float, float32, short, uint16), but {} was given'.format(dtype)
+        raise Exception(errstr)
 
-    # Set some parameters
-    par.auto_parameters = False
-    par.rel_error = rel_error
-    par.gradient_smoothing = gradient_smoothing
     converter.set_parameters(par)
     converter.set_verbose(verbose)
 
-    # Compute APR and sample particle values
-
+    # launch interactive APR converter
     io_int.interactive_apr(converter, apr, image)
 
     if verbose:
@@ -35,6 +41,7 @@ def get_apr_interactive(image, dtype='float', rel_error=0.1, gradient_smoothing=
         print("Compuational Ratio: {:7.2f}".format(cr))
         print("Sampling particles ... \n")
 
+    # sample particles
     parts.sample_image(apr, image)
 
     return apr, parts
