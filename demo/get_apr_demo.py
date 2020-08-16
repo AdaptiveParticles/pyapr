ddf4756d1a004d1bf8e4645c07367353553ad039
joeljonsson <jonsson@mpi-cbg.de>
2020-08-16 17:49:54 +0200

    cleaned up demos

--- demo/get_apr_demo.py
+++ demo/get_apr_demo.py
@@ -14,13 +14,11 @@ def main():
     while img.ndim < 3:
         img = np.expand_dims(img, axis=0)
 
-    # Initialize APRParameters (only Ip_th, grad_th and sigma_th are set interactively)
+    # Initialize and set some APRParameters (only Ip_th, grad_th and sigma_th are set interactively)
     par = pyapr.APRParameters()
     par.auto_parameters = False
     par.rel_error = 0.1
-    par.gradient_smoothing = 2
-    par.min_signal = 200
-    par.noise_sd_estimate = 5
+    par.gradient_smoothing = 3
 
     # Compute APR and sample particle values
     apr, parts = pyapr.converter.get_apr_interactive(img, dtype=img.dtype, params=par, verbose=True)
@@ -31,29 +29,21 @@ def main():
     # Write the resulting APR to file
     print("Writing APR to file ... \n")
     fpath_apr = io_int.save_apr_file_name()  # get path through gui
+    pyapr.io.write(fpath_apr, apr, parts)
 
-    # Initialize APRFile for I/O
-    aprfile = pyapr.io.APRFile()
-    aprfile.set_read_write_tree(True)
+    # Display the size of the file
+    file_sz = os.path.getsize(fpath_apr)
+    print("APR File Size: {:7.2f} MB \n".format(file_sz * 1e-6))
 
-    # Write APR and particles to file
-    aprfile.open(fpath_apr, 'WRITE')
-    aprfile.write_apr(apr)
-    aprfile.write_particles('particles', parts)
-
-    # Compute compression and computational ratios
-    file_sz = aprfile.current_file_size_MB()
-    print("APR File Size: {:7.2f} MB \n".format(file_sz))
-
-    mcr = os.path.getsize(fpath) * 1e-6 / file_sz
-    cr = img.size/apr.total_number_particles()
-
+    # Compute compression ratio
+    mcr = os.path.getsize(fpath) / file_sz
     print("Memory Compression Ratio: {:7.2f}".format(mcr))
+
+    # Compute computational ratio
+    cr = img.size/apr.total_number_particles()
     print("Compuational Ratio: {:7.2f}".format(cr))
 
-    aprfile.close()
-
-    print("Done. \n")
+    print("Done.")
 
 
 if __name__ == '__main__':
--- demo/reconstruction_demo.py
+++ demo/reconstruction_demo.py
@@ -5,27 +5,23 @@ from skimage import io as skio
 
 def main():
 
+    # get input APR file path from gui
     io_int = pyapr.filegui.InteractiveIO()
-    fpath_apr = io_int.get_apr_file_name()  # get APR file path from gui
-
-    aprfile = pyapr.io.APRFile()
-    aprfile.set_read_write_tree(True)
+    fpath_apr = io_int.get_apr_file_name()
 
     # Initialize APR and particle objects
     parts = pyapr.ShortParticles()
     apr = pyapr.APR()
 
-    # Read from APR file
-    aprfile.open(fpath_apr, 'READ')
-    aprfile.read_apr(apr)
-    aprfile.read_particles(apr, 'particles', parts)
-    aprfile.close()
+    # Read APR and particles from file
+    pyapr.io.read(fpath_apr, apr, parts)
 
+    # Compute piecewise constant, smooth and level reconstructions
     recon = np.array(pyapr.numerics.reconstruction.recon_pc(apr, parts), copy=False)
     smooth_recon = np.array(pyapr.numerics.reconstruction.recon_smooth(apr, parts), copy=False)
     level_recon = np.array(pyapr.numerics.reconstruction.recon_level(apr), copy=False)
 
-    # save the results
+    # Save the results
     file_name = fpath_apr.split('/')[-1]
     file_name = file_name.split('.')[:-1]
     file_name = '.'.join(file_name)
