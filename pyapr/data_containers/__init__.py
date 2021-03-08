eafc7f57f0d27d0fda31fd80caabab0984add262
Jules Scholler <jules.scholler@gmail.com>
2021-03-08 14:59:32 +0100

    Move APRSlicer into data_container and remove Napari dependency from pyapr.

--- pyapr/data_containers/__init__.py
+++ pyapr/data_containers/__init__.py
@@ -1,1 +1,3 @@
 from _pyaprwrapper.data_containers import *
+
+from .PyAPRSlicer import APRSlicer
--- pyapr/viewer/__init__.py
+++ pyapr/viewer/__init__.py
@@ -4,5 +4,3 @@ from .partsViewer import *
 from .compressInteractive import *
 from .raycastViewer import *
 from .particleScatterPlot import particle_scatter_plot
-
-from ._apr_napari_layer import apr_to_napari_Image, apr_to_napari_Labels
