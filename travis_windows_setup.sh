854acad0af8966925d9e8a9cd7a383d18452b5a3
cheesema <cheesema@mpi-cbg.de>
2021-04-24 14:59:59 +0100

    Trying to get the external vars working

--- travis_windows_setup.sh
+++ travis_windows_setup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -x
-mkdir build
-cd build
 git clone https://github.com/microsoft/vcpkg
 cd vcpkg
 ./bootstrap-vcpkg.bat
-./vcpkg.exe install blosc:x64-windows gtest:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
+#./vcpkg.exe install blosc:x64-windows gtest:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
 cd ..
 
