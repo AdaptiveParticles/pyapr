72d6e101e96df78d1fd63dbbba62e73b50858919
cheesema <cheesema@mpi-cbg.de>
2021-04-24 15:21:30 +0100

    try again

--- travis_windows_setup.sh
+++ travis_windows_setup.sh
@@ -3,6 +3,6 @@ set -x
 git clone https://github.com/microsoft/vcpkg
 cd vcpkg
 ./bootstrap-vcpkg.bat
-#./vcpkg.exe install blosc:x64-windows gtest:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
+./vcpkg.exe install blosc:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
 cd ..
 
