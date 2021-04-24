9f8f927cdf4510c1c63e954083002021049d8091
cheesema <cheesema@mpi-cbg.de>
2021-04-24 15:57:10 +0100

    Trying to add cache and update cmake

--- travis_windows_setup.sh
+++ travis_windows_setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -x
+cd ..
+cd ..
 git clone https://github.com/microsoft/vcpkg
 cd vcpkg
-./bootstrap-vcpkg.bat
-./vcpkg.exe install blosc:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
-cd ..
+#./bootstrap-vcpkg.bat
+#./vcpkg.exe install blosc:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
+cd AdaptiveParticles/PyLibAPR
 
