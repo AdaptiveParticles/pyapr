cbb3983a66901c8aa800a133752c8aa929fac555
cheesema <cheesema@mpi-cbg.de>
2021-04-25 11:11:18 +0100

    Now not using CIBuild for windows right now, just to see if I can get it working properly

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='PyLibAPR-test',
-    version='0.1.4',
+    version='0.1.5',
     ext_modules=[CMakeExtension('_pyaprwrapper')],
     cmdclass={
         'build_ext': CMakeBuild,
