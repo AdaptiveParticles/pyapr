9ce4ef8a65b1e943e5cbc2cff5c9d59ac54ba38e
Jules Scholler <jules.scholler@wysscenter.ch>
2021-08-18 09:50:31 +0200

    modify max coords order so that it is consistent with numpy ordering.

--- pyapr/numerics/transform/morphological.py
+++ pyapr/numerics/transform/morphological.py
@@ -99,7 +99,7 @@ def find_objects(apr: pyapr.APR,
     max_coords = np.zeros((max_label+1, 3), dtype=np.int32)
     pyapr.numerics.transform.find_objects_cpp(apr, labels, min_coords, max_coords)
 
-    max_coords[0, :] = [apr.org_dims(x) for x in range(3)]
+    max_coords[0, :] = [apr.org_dims(x) for x in [2, 0, 1]]
     min_coords[0, :] = 0
 
     return min_coords, max_coords
