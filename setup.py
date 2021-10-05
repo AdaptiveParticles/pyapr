04bbc88b649736b1cb5926213a6b0918b51e736f
cheesema <cheesema@mpi-cbg.de>
2021-10-05 22:29:32 +0100

    Bump version and deploy

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ class CMakeBuild(build_ext):
 setup(
     name='pyapr',
 
-    version='0.0.0.3',
+    version='0.0.0.4',
     ext_modules=[CMakeExtension('_pyaprwrapper')],
     cmdclass={
         'build_ext': CMakeBuild,
