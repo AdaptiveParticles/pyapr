b9b0d2c7f698b7d84e360258cf60abc74c4e60d2
joeljonsson <jonsson@mpi-cbg.de>
2022-04-27 01:02:23 +0200

    fuse median2D and median3D -> median_filter

--- pyapr/numerics/filter/__init__.py
+++ pyapr/numerics/filter/__init__.py
@@ -1,3 +1,3 @@
 from _pyaprwrapper.numerics.filter import *
 from .stencils import *
-from .median import median_filter_2d, median_filter_3d
+from .median import median_filter
--- pyapr/numerics/filter/median.py
+++ pyapr/numerics/filter/median.py
@@ -1,26 +1,21 @@
 from _pyaprwrapper.numerics.filter import *
 import pyapr
+from typing import Tuple
 
-allowed_sizes = [3, 5, 7, 9, 11]
+__allowed_sizes_median__ = [(x, x, x) for x in [3, 5, 7, 9, 11]] + [(1, x, x) for x in [3, 5, 7, 9, 11]]
 
 
-def median_filter_3d(apr, parts, size):
-    if size not in allowed_sizes:
-        raise ValueError('median_filter_3d received an invalid argument \'size\'. Allowed values are '
-                         '3, 5, 7, 9 and 11')
+def median_filter(apr: pyapr.APR,
+                  parts: [pyapr.ShortParticles,
+                          pyapr.FloatParticles],
+                  size: Tuple[int, int, int] = (5, 5, 5)):
 
-    fname = 'median_filter_{}{}{}'.format(size, size, size)
+    if size not in __allowed_sizes_median__:
+        raise ValueError(f'median_filter received an invalid argument \'size\' with value {size}. '
+                         f'Allowed values are \n\t{__allowed_sizes_median__}')
+
+    fname = 'median_filter_{}{}{}'.format(*size)
     output = pyapr.ShortParticles() if isinstance(parts, pyapr.ShortParticles) else pyapr.FloatParticles()
     globals()[fname](apr, parts, output)
     return output
 
-
-def median_filter_2d(apr, parts, size):
-    if size not in allowed_sizes:
-        raise ValueError('median_filter_2d received an invalid argument \'size\'. Allowed values are '
-                         '3, 5, 7, 9 and 11')
-
-    fname = 'median_filter_1{}{}'.format(size, size)
-    output = pyapr.ShortParticles() if isinstance(parts, pyapr.ShortParticles) else pyapr.FloatParticles()
-    globals()[fname](apr, parts, output)
-    return output
