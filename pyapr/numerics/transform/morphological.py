37ef2bd92821c2c195d6c55282374a9b838c127d
joeljonsson <jonsson@mpi-cbg.de>
2022-04-28 16:45:06 +0200

    add docstrings to morphology functions

--- pyapr/numerics/transform/morphological.py
+++ pyapr/numerics/transform/morphological.py
@@ -12,13 +12,32 @@ def opening(apr: APR,
             binary: bool = False,
             radius: int = 1,
             inplace: bool = False) -> Union[ShortParticles, LongParticles, FloatParticles]:
+    """
+    Apply morphological opening (erosion followed by dilation) to an APR image. Only considers face-side neighbors
+    (6-connectivity in 3D). If `radius > 1` the erosion and dilation are repeated `radius` times.
+
+    Parameters
+    ----------
+    apr: APR
+        Input APR data structure.
+    parts: ShortParticles, LongParticles, FloatParticles
+        Input particle values.
+    binary: bool
+        If `True`, binary erosion and dilation is used. Otherwise `min` and `max` operators are used. (default: False)
+    radius: int
+        Radius of the erosion and dilation operations. The 6-connective operations are iterated `radius` times. (default: 1)
+    inplace: bool
+        If `True`, the operation modifies the input `parts`. Otherwise, a copy of the input is used. (default: False)
+
+    Returns
+    -------
+    parts_copy: ShortParticles, LongParticles, FloatParticles
+        Output particle values of the same type as the input `parts`.
+    """
 
     parts_copy = parts if inplace else parts.copy()
-
-    # morphological opening
     erosion(apr, parts_copy, binary=binary, radius=radius)
     dilation(apr, parts_copy, binary=binary, radius=radius)
-
     return parts_copy
 
 
@@ -27,13 +46,32 @@ def closing(apr: APR,
             binary: bool = False,
             radius: int = 1,
             inplace: bool = False) -> Union[ShortParticles, LongParticles, FloatParticles]:
+    """
+    Apply morphological closing (dilation followed by erosion) to an APR image. Only considers face-side neighbors
+    (6-connectivity in 3D). If `radius > 1` the erosion and dilation are repeated `radius` times.
+
+    Parameters
+    ----------
+    apr: APR
+        Input APR data structure.
+    parts: ShortParticles, LongParticles, FloatParticles
+        Input particle values.
+    binary: bool
+        If `True`, binary erosion and dilation is used. Otherwise `min` and `max` operators are used. (default: False)
+    radius: int
+        Radius of the erosion and dilation operations. The 6-connective operations are iterated `radius` times. (default: 1)
+    inplace: bool
+        If `True`, the operation modifies the input `parts`. Otherwise, a copy of the input is used. (default: False)
+
+    Returns
+    -------
+    parts_copy: ShortParticles, LongParticles, FloatParticles
+        Output particle values of the same type as the input `parts`.
+    """
 
     parts_copy = parts if inplace else parts.copy()
-
-    # morphological closing
     dilation(apr, parts_copy, binary=binary, radius=radius)
     erosion(apr, parts_copy, binary=binary, radius=radius)
-
     return parts_copy
 
 
@@ -41,6 +79,29 @@ def tophat(apr: APR,
            parts: Union[ShortParticles, LongParticles, FloatParticles],
            binary: bool = False,
            radius: int = 1) -> Union[ShortParticles, LongParticles, FloatParticles]:
+    """
+    Apply top-hat transform to an APR image by subtracting an opening of the image.
+
+    Parameters
+    ----------
+    apr: APR
+        Input APR data structure.
+    parts: ShortParticles, LongParticles, FloatParticles
+        Input particle values.
+    binary: bool
+        Use binary opening? (default: False)
+    radius: int
+        Radius of the opening operation. (default: 1)
+
+    Returns
+    -------
+    out: ShortParticles, LongParticles, FloatParticles
+        Output particle values of the same type as the input `parts`.
+
+    See also
+    --------
+    pyapr.numerics.transform.opening
+    """
 
     tmp = opening(apr, parts, binary=binary, radius=radius, inplace=False)
     return parts - tmp
@@ -50,40 +111,95 @@ def bottomhat(apr: APR,
               parts: Union[ShortParticles, LongParticles, FloatParticles],
               binary: bool = False,
               radius: int = 1) -> Union[ShortParticles, LongParticles, FloatParticles]:
+    """
+    Apply bottom-hat transform to an APR image by subtracting it from a closing of itself.
 
-    # morphological closing
+    Parameters
+    ----------
+    apr: APR
+        Input APR data structure.
+    parts: ShortParticles, LongParticles, FloatParticles
+        Input particle values.
+    binary: bool
+        Use binary closing? (default: False)
+    radius: int
+        Radius of the closing operation. (default: 1)
+
+    Returns
+    -------
+    out: ShortParticles, LongParticles, FloatParticles
+        Output particle values of the same type as the input `parts`.
+
+    See also
+    --------
+    pyapr.numerics.transform.closing
+    """
+
     tmp = closing(apr, parts, binary=binary, radius=radius, inplace=False)
-
-    # return difference
     return tmp - parts
 
 
 def remove_small_holes(apr: APR,
-                       parts: Union[ShortParticles, LongParticles],
+                       labels: Union[ShortParticles, LongParticles],
                        min_volume: int = 200) -> Union[ShortParticles, LongParticles]:
+    """
+    Remove holes smaller than a threshold from an input particle mask. Assumes that the background value is 0.
 
-    mask = parts < 1
+    Parameters
+    ----------
+    apr: APR
+        Input APR data structure.
+    labels: ShortParticles, LongParticles
+        Input particle mask.
+    min_volume: int
+        Remove holes smaller in volume than `min_volume` voxels. (default: 200)
+
+    Returns
+    -------
+    out: ShortParticles, LongParticles
+        The mask with holes removed. If the input mask was binary, a binary mask is returned. Otherwise, connected
+        components are recomputed.
+    """
+
+    mask = labels < 1
     cc_inverted = ShortParticles()
     connected_component(apr, mask, cc_inverted)
     remove_small_objects(apr, cc_inverted, min_volume=min_volume)
     mask = cc_inverted < 1
 
-    if parts.max() > 1:
+    if labels.max() > 1:
         # Case where input is a label map
-        if isinstance(parts, ShortParticles):
+        if isinstance(labels, ShortParticles):
             cc = ShortParticles()
-        elif isinstance(parts, LongParticles):
+        elif isinstance(labels, LongParticles):
             mask = np.array(mask).astype('uint16')
             mask = ShortParticles(mask)
             cc = LongParticles()
         connected_component(apr, mask, cc)
         return cc
-    else:
-        return mask
+    return mask
 
 
 def find_objects(apr: APR,
                  labels: Union[ShortParticles, LongParticles]) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Find and return tight bounding boxes for each unique input label. Assumes that the labels are
+    ordered from 0, such that 0 is background and each value > 0 corresponds to a connected component.
+
+    Parameters
+    ----------
+    apr: APR
+        Input APR data structure.
+    labels: ShortParticles or LongParticles
+        Input (object) labels.
+
+    Returns
+    -------
+    min_coords: numpy.ndarray
+        Array of shape `(labels.max() + 1, 3)` containing the "lower" corner of each bounding box in z, x and y.
+    max_coords: numpy.ndarray
+        Array of shape `(labels.max() + 1, 3)` containing the "upper" corner of each bounding box in z, x and y.
+    """
 
     max_label = labels.max()
     max_dim = max([apr.org_dims(x) for x in range(3)])
@@ -100,6 +216,23 @@ def find_objects(apr: APR,
 def find_label_centers(apr: APR,
                        labels: Union[ShortParticles, LongParticles],
                        weights: Optional[Union[ShortParticles, FloatParticles]] = None) -> np.ndarray:
+    """
+    Compute the volumetric center of each unique input label, optionally weighted by, e.g., image intensity.
+
+    Parameters
+    ----------
+    apr: APR
+        Input APR data structure.
+    labels: ShortParticles or LongParticles
+        Input (object) labels.
+    weights: ShortParticles or FloatParticles, optional
+        (optional) Weight for each particle. Normalization is applied internally. (default: None)
+
+    Returns
+    -------
+    coords: numpy.ndarray
+        Array containing the center coordinates.
+    """
 
     max_label = labels.max()
     coords = np.zeros((max_label+1, 3), dtype=np.float64)
@@ -112,9 +245,23 @@ def find_label_centers(apr: APR,
 
 def find_label_volume(apr: APR,
                       labels: Union[ShortParticles, LongParticles]) -> np.ndarray:
+    """
+    Return the volume (in voxels) of each unique input label.
+
+    Parameters
+    ----------
+    apr: APR
+        Input APR data structure.
+    labels: ShortParticles or LongParticles
+        Input (object) labels.
+
+    Returns
+    -------
+    volume: numpy.ndarray
+        Array of shape `(labels.max() + 1,)` containing the label volumes.
+    """
 
     max_label = labels.max()
     volume = np.zeros((max_label+1), dtype=np.uint64)
     find_label_volume_cpp(apr, labels, volume)
-
     return volume
