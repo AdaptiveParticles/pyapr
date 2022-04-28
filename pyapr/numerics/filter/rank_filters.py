33762a309bde462c2a4ac2b886d0ac985f944f66
joeljonsson <jonsson@mpi-cbg.de>
2022-04-28 12:16:51 +0200

    explicit imports, improve typing

--- pyapr/numerics/filter/rank_filters.py
+++ pyapr/numerics/filter/rank_filters.py
@@ -1,15 +1,14 @@
 from _pyaprwrapper.numerics.filter import *
-import pyapr
-from typing import Tuple
+from ...data_containers import APR, ShortParticles, FloatParticles
+from typing import Tuple, Union
 
 __allowed_sizes_median__ = [(x, x, x) for x in [3, 5, 7, 9, 11]] + [(1, x, x) for x in [3, 5, 7, 9, 11]]
 __allowed_sizes_min__ = [(x, x, x) for x in [3, 5]] + [(1, x, x) for x in [3, 5]]
 __allowed_sizes_max__ = __allowed_sizes_min__
 
 
-def median_filter(apr: pyapr.APR,
-                  parts: [pyapr.ShortParticles,
-                          pyapr.FloatParticles],
+def median_filter(apr: APR,
+                  parts: Union[ShortParticles, FloatParticles],
                   size: Tuple[int, int, int] = (5, 5, 5)):
     """
     Apply median filter to an APR image and return a new set of particle values.
@@ -19,18 +18,18 @@ def median_filter(apr: pyapr.APR,
 
     Parameters
     ----------
-    apr: pyapr.APR
-        Input APR data structure
-    parts: [pyapr.ShortParticles, pyapr.FloatParticles]
-        Input particle values
+    apr: APR
+        APR data structure.
+    parts: ShortParticles or FloatParticles
+        Input particle values.
     size: (int, int, int)
         Size of the neighborhood in (z, x, y) dimensions.
         Allowed sizes are (x, x, x) and (1, x, x) for x in [3, 5, 7, 9, 11].
 
     Returns
     -------
-    output: [pyapr.ShortParticles, pyapr.FloatParticles]
-        Median filtered particle values of the same type as the input
+    output: ShortParticles or FloatParticles
+        Median filtered particle values of the same type as the input.
     """
 
     if size not in __allowed_sizes_median__:
@@ -38,14 +37,13 @@ def median_filter(apr: pyapr.APR,
                          f'Allowed values are \n\t{__allowed_sizes_median__}')
 
     fname = 'median_filter_{}{}{}'.format(*size)
-    output = pyapr.ShortParticles() if isinstance(parts, pyapr.ShortParticles) else pyapr.FloatParticles()
+    output = ShortParticles() if isinstance(parts, ShortParticles) else FloatParticles()
     globals()[fname](apr, parts, output)
     return output
 
 
-def min_filter(apr: pyapr.APR,
-               parts: [pyapr.ShortParticles,
-                       pyapr.FloatParticles],
+def min_filter(apr: APR,
+               parts: Union[ShortParticles, FloatParticles],
                size: Tuple[int, int, int] = (5, 5, 5)):
     """
     Apply minimum filter to an APR image and return a new set of particle values.
@@ -55,32 +53,31 @@ def min_filter(apr: pyapr.APR,
 
     Parameters
     ----------
-    apr: pyapr.APR
-        Input APR data structure
-    parts: [pyapr.ShortParticles, pyapr.FloatParticles]
-        Input particle values
+    apr: APR
+        APR data structure.
+    parts: ShortParticles or FloatParticles
+        Input particle values.
     size: (int, int, int)
         Size of the neighborhood in (z, x, y) dimensions.
         Allowed values are (x, x, x) and (1, x, x) for x in [3, 5].
 
     Returns
     -------
-    output: [pyapr.ShortParticles, pyapr.FloatParticles]
-        Minimum filtered particle values of the same type as the input
+    output: ShortParticles or FloatParticles
+        Minimum filtered particle values of the same type as the input.
     """
     if size not in __allowed_sizes_min__:
         raise ValueError(f'min_filter received an invalid argument \'size\' = {size}. '
                          f'Allowed values are \n\t{__allowed_sizes_min__}')
 
     fname = 'min_filter_{}{}{}'.format(*size)
-    output = pyapr.ShortParticles() if isinstance(parts, pyapr.ShortParticles) else pyapr.FloatParticles()
+    output = ShortParticles() if isinstance(parts, ShortParticles) else FloatParticles()
     globals()[fname](apr, parts, output)
     return output
 
 
-def max_filter(apr: pyapr.APR,
-               parts: [pyapr.ShortParticles,
-                       pyapr.FloatParticles],
+def max_filter(apr: APR,
+               parts: Union[ShortParticles, FloatParticles],
                size: Tuple[int, int, int] = (5, 5, 5)):
     """
     Apply maximum filter to an APR image and return a new set of particle values.
@@ -90,9 +87,9 @@ def max_filter(apr: pyapr.APR,
 
     Parameters
     ----------
-    apr: pyapr.APR
-        Input APR data structure
-    parts: [pyapr.ShortParticles, pyapr.FloatParticles]
+    apr: APR
+        APR data structure.
+    parts: ShortParticles or FloatParticles
         Input particle values
     size: (int, int, int)
         Size of the neighborhood in (z, x, y) dimensions.
@@ -100,8 +97,8 @@ def max_filter(apr: pyapr.APR,
 
     Returns
     -------
-    output: [pyapr.ShortParticles, pyapr.FloatParticles]
-        Maximum filtered particle values of the same type as the input
+    output: ShortParticles or FloatParticles
+        Maximum filtered particle values of the same type as the input.
     """
 
     if size not in __allowed_sizes_max__:
@@ -109,7 +106,7 @@ def max_filter(apr: pyapr.APR,
                          f'Allowed values are \n\t{__allowed_sizes_max__}')
 
     fname = 'max_filter_{}{}{}'.format(*size)
-    output = pyapr.ShortParticles() if isinstance(parts, pyapr.ShortParticles) else pyapr.FloatParticles()
+    output = ShortParticles() if isinstance(parts, ShortParticles) else FloatParticles()
     globals()[fname](apr, parts, output)
     return output
 
