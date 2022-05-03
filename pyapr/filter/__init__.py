4c3ea9b4cdda6a589f811c9b48949a58a291d490
joeljonsson <jonsson@mpi-cbg.de>
2022-05-04 01:21:17 +0200

    add gradient wrapper functions

--- pyapr/filter/__init__.py
+++ pyapr/filter/__init__.py
@@ -1,3 +1,4 @@
 from .convolution import convolve, correlate
+from .gradient import gradient, gradient_magnitude, sobel, sobel_magnitude
 from .stencils import *
 from .rank_filters import median_filter, min_filter, max_filter
