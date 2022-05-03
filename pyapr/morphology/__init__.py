a681810edd0dbd612efcbda2f253e93d340c0110
joeljonsson <jonsson@mpi-cbg.de>
2022-05-03 12:22:15 +0200

    add and expose wrapper functions for morphology operations

--- pyapr/morphology/__init__.py
+++ pyapr/morphology/__init__.py
@@ -1,1 +1,2 @@
-from .morphology import *
+from .morphology import dilation, erosion, opening, closing, tophat, bottomhat, find_perimeter, remove_small_objects, \
+                        remove_large_objects, remove_edge_objects, remove_small_holes
