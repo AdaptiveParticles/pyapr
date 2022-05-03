9373b8485a2fa9cb90120d817495580bec78ffec
joeljonsson <jonsson@mpi-cbg.de>
2022-05-04 01:51:29 +0200

    update numerics demo

--- demo/numerics_demo.py
+++ demo/numerics_demo.py
@@ -11,15 +11,15 @@ fpath_apr = io_int.get_apr_file_name()      # get APR file path from gui
 # Read from APR file
 apr, parts = pyapr.io.read(fpath_apr)
 
-# Compute gradient along a dimension (Sobel filter). dimension can be 0, 1 or 2
+# Compute gradient along a dimension (central finite differences).
 output = pyapr.filter.gradient(apr, parts, dim=0, delta=1.0)
-pyapr.viewer.parts_viewer(apr, output)   # Display the result
+pyapr.viewer.parts_viewer(apr, output)
 
-# Compute gradient magnitude (central finite differences)
+# Compute gradient magnitude (Sobel filters)
 par = apr.get_parameters()
 output = pyapr.filter.sobel_magnitude(apr, parts, deltas=(par.dy, par.dx, par.dz), output=output)
-pyapr.viewer.parts_viewer(apr, output)  # Display the result
+pyapr.viewer.parts_viewer(apr, output)
 
-# # Compute local standard deviation around each particle
-# pyapr.filter.local_std(apr, parts, output, size=(5, 5, 5))
-# pyapr.viewer.parts_viewer(apr, output)  # Display the result
+# Compute local standard deviation around each particle
+pyapr.filter.std(apr, parts, size=5, output=output)
+pyapr.viewer.parts_viewer(apr, output)
