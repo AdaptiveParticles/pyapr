fa210c5b4d1572c56d33fff3cefaab0ff12510a7
joeljonsson <jonsson@mpi-cbg.de>
2022-05-04 13:59:53 +0200

    update richardson-lucy demo

--- demo/richardson_lucy_demo.py
+++ demo/richardson_lucy_demo.py
@@ -21,37 +21,27 @@ parts += offset
 
 # Specify the PSF and number of iterations
 psf = pyapr.filter.get_gaussian_stencil(size=5, sigma=1, ndims=3, normalize=True)
-niter = 10
 
 # Richardson-lucy deconvolution
 t0 = time()
-output = pyapr.FloatParticles()
-pyapr.restoration.richardson_lucy(apr, parts, output, psf, niter, use_stencil_downsample=True,
-                                  normalize_stencil=True, resume=False)
+output = pyapr.restoration.richardson_lucy(apr, parts, psf, num_iter=10)
 print('RL took {} seconds'.format(time()-t0))
-
 
 # Using total variation regularization
 t0 = time()
-output_tv = pyapr.FloatParticles()
-reg_factor = 1e-2
-pyapr.restoration.richardson_lucy_tv(apr, parts, output_tv, psf, niter, reg_factor, use_stencil_downsample=True,
-                                     normalize_stencil=True, resume=False)
+output_tv = pyapr.restoration.richardson_lucy_tv(apr, parts, psf, reg_param=1e-2, num_iter=10)
 print('RLTV took {} seconds'.format(time()-t0))
 
-
-# Alternatively, if PyLibAPR is built with CUDA enabled and psf is of size (3, 3, 3) or (5, 5, 5)
+# if pyapr is built with CUDA enabled and psf is of size (3, 3, 3) or (5, 5, 5)
 cuda = False
 if pyapr.cuda_enabled() and psf.shape in [(3, 3, 3), (5, 5, 5)]:
     t0 = time()
-    output_cuda = pyapr.FloatParticles()
-    pyapr.restoration.richardson_lucy_cuda(apr, parts, output_cuda, psf, niter, use_stencil_downsample=True,
-                                           normalize_stencil=True, resume=False)
+    output_cuda = pyapr.restoration.richardson_lucy_cuda(apr, parts, psf, num_iter=10)
     print('RL cuda took {} seconds'.format(time()-t0))
     cuda = True
 
 
-# Display the result
+# Display the results
 pyapr.viewer.parts_viewer(apr, output)
 pyapr.viewer.parts_viewer(apr, output_tv)
 if cuda:
