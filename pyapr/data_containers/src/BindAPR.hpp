4427fdece0f220f833ca557acf84d87a6924a44f
joeljonsson <jonsson@mpi-cbg.de>
2022-05-18 18:25:34 +0200

    add arguments annotations to APR methods

--- pyapr/data_containers/src/BindAPR.hpp
+++ pyapr/data_containers/src/BindAPR.hpp
@@ -17,6 +17,9 @@ namespace py = pybind11;
 PYBIND11_MAKE_OPAQUE(std::vector<APR*>)
 
 void AddAPR(pybind11::module &m, const std::string &modulename) {
+
+    using namespace py::literals;
+
     py::class_<APR>(m, modulename.c_str())
             .def(py::init())
             .def("__repr__", [](APR& a) {
@@ -27,12 +30,12 @@ void AddAPR(pybind11::module &m, const std::string &modulename) {
             .def("total_number_tree_particles", &APR::total_number_tree_particles, "return number of interior tree particles")
             .def("level_min", &APR::level_min, "return the minimum resolution level")
             .def("level_max", &APR::level_max, "return the maximum resolution level")
-            .def("x_num", &APR::x_num,  "Gives the maximum bounds in the x direction for the given level")
-            .def("y_num", &APR::y_num,  "Gives the maximum bounds in the y direction for the given level")
-            .def("z_num", &APR::z_num,  "Gives the maximum bounds in the z direction for the given level")
+            .def("x_num", &APR::x_num,  "Gives the maximum bounds in the x direction for the given level", "level"_a)
+            .def("y_num", &APR::y_num,  "Gives the maximum bounds in the y direction for the given level", "level"_a)
+            .def("z_num", &APR::z_num,  "Gives the maximum bounds in the z direction for the given level", "level"_a)
             .def("iterator", &APR::iterator, "Return a linear iterator for APR particles")
             .def("tree_iterator", &APR::tree_iterator, "Return a linear iterator for interior APRTree particles")
-            .def("org_dims", &APR::org_dims, "returns the original image size in a specified dimension (y, x, z)")
+            .def("org_dims", &APR::org_dims, "returns the original image size in a specified dimension (y, x, z)" , "dim"_a)
             .def("shape", [](APR& self){return py::make_tuple(self.org_dims(2), self.org_dims(1), self.org_dims(0));}, "returns the original pixel image dimensions as a tuple (z, x, y)")
             .def("get_parameters", &APR::get_apr_parameters, "return the parameters used to create the APR")
             .def("computational_ratio", &APR::computational_ratio, "return the computational ratio (number of pixels in original image / number of particles in the APR)");
