ae6c667676eca84e35ca143a1cb7238b5bf918c5
joeljonsson <jonsson@mpi-cbg.de>
2022-05-19 20:01:54 +0200

    edit method descriptions

--- pyapr/data_containers/src/BindLinearIterator.hpp
+++ pyapr/data_containers/src/BindLinearIterator.hpp
@@ -17,19 +17,23 @@ namespace py = pybind11;
 
 // -------- wrapper -------------------------------------------------
 void AddLinearIterator(pybind11::module &m) {
+
+    using namespace py::literals;
+
     py::class_<LinearIterator>(m, "LinearIterator")
             .def(py::init())
-            .def("total_number_particles", &LinearIterator::total_number_particles, "return number of particles",
-                 py::arg("level")=0)
+            .def("total_number_particles", &LinearIterator::total_number_particles,
+                 "return number of particles up to level (by default level = 0 -> all levels)", "level"_a=0)
             .def("level_min", &LinearIterator::level_min, "return the minimum resolution level")
             .def("level_max", &LinearIterator::level_max, "return the maximum resolution level")
-            .def("x_num", &LinearIterator::x_num,  "Gives the maximum bounds in the x direction for the given level")
-            .def("y_num", &LinearIterator::y_num,  "Gives the maximum bounds in the y direction for the given level")
-            .def("z_num", &LinearIterator::z_num,  "Gives the maximum bounds in the z direction for the given level")
-            .def("y", &LinearIterator::get_y, "returns the y-coordinate of a given particle index")
-            .def("begin", &LinearIterator::begin, "returns the index of the first particle at a given combination of level, z, and x",
-                        py::arg("level"), py::arg("z"), py::arg("z"))
-            .def("end", &LinearIterator::end, "returns the end index of the current row (l, z, x combination)");
+            .def("x_num", &LinearIterator::x_num,  "Gives the maximum bounds in the x direction for the given level", "level"_a)
+            .def("y_num", &LinearIterator::y_num,  "Gives the maximum bounds in the y direction for the given level", "level"_a)
+            .def("z_num", &LinearIterator::z_num,  "Gives the maximum bounds in the z direction for the given level", "level"_a)
+            .def("y", &LinearIterator::get_y, "returns the y-coordinate of a given particle index", "idx"_a)
+            .def("begin", &LinearIterator::begin,
+                 "returns the index of the first particle in the sparse row (level, z, x)", "level"_a, "z"_a, "x"_a)
+            .def("end", &LinearIterator::end,
+                 "returns the (exclusive) end index of the current sparse row (level, z, x) opened using 'begin'");
 }
 
 #endif //PYLIBAPR_PYLINEARITERATOR_HPP
