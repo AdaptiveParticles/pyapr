805c107e7ddf06f9d0333abdab73885baaf232c0
joeljonsson <jonsson@mpi-cbg.de>
2022-05-25 15:18:19 +0200

    fix error when inputting a list of allowed types

--- pyapr/_common.py
+++ pyapr/_common.py
@@ -6,7 +6,7 @@ def _check_input(apr: APR,
                  parts: Union[ByteParticles, ShortParticles, FloatParticles, LongParticles],
                  allowed_types: Optional[Union[List, Tuple]] = None):
     if allowed_types:
-        assert isinstance(parts, allowed_types), \
+        assert isinstance(parts, tuple(allowed_types)), \
             TypeError(f'Input particles must be of type {allowed_types}, got {type(parts)}.')
     assert apr.total_number_particles() > 0, ValueError(f'Input APR is not initialized.')
     assert len(parts) == apr.total_number_particles(), \
@@ -17,7 +17,7 @@ def _check_input_tree(apr: APR,
                       tree_parts: Union[ByteParticles, ShortParticles, FloatParticles, LongParticles],
                       allowed_types: Optional[Union[List, Tuple]] = None):
     if allowed_types:
-        assert isinstance(tree_parts, allowed_types), \
+        assert isinstance(tree_parts, tuple(allowed_types)), \
             TypeError(f'Input tree particles must be of type {allowed_types}, got {type(tree_parts)}.')
     assert apr.total_number_particles() > 0, ValueError(f'Input APR is not initialized.')
     assert len(tree_parts) == apr.total_number_tree_particles(), \
