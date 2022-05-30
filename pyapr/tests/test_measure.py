5ce8d6f9e33b85b808a614e954f590a3848994e2
joeljonsson <jonsson@mpi-cbg.de>
2022-05-31 01:33:56 +0200

    update test with changes to find_label_centers

--- pyapr/tests/test_measure.py
+++ pyapr/tests/test_measure.py
@@ -28,13 +28,13 @@ def test_measures(mask_type):
     # compute the (weighted) volumetric center of each object
     obj_centers = pyapr.measure.find_label_centers(apr, cc)
     obj_centers_weighted = pyapr.measure.find_label_centers(apr, cc, weights=parts)
-    assert obj_centers.shape == obj_centers_weighted.shape == (2, 3)
+    assert obj_centers.shape == obj_centers_weighted.shape == (3, 3)
 
     # check that object centers are within the bounding boxes computed by `find_objects`
-    for i in range(obj_centers.shape[0]):
+    for i in (1, 2):
         for j in range(3):
-            assert min_coords[i+1, j] < obj_centers[i, j] < max_coords[i+1, j]
-            assert min_coords[i+1, j] < obj_centers_weighted[i, j] < max_coords[i+1, j]
+            assert min_coords[i, j] < obj_centers[i, j] < max_coords[i, j]
+            assert min_coords[i, j] < obj_centers_weighted[i, j] < max_coords[i, j]
 
     # compute the volume of each object
     vol = pyapr.measure.find_label_volume(apr, cc)
