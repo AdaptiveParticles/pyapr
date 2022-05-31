a1ccdf6abfdc6c2f68d4a859ba4c4143e7714388
joeljonsson <jonsson@mpi-cbg.de>
2022-05-31 02:58:43 +0200

    print difference to debug failing tests on macos

--- pyapr/tests/test_segmentation.py
+++ pyapr/tests/test_segmentation.py
@@ -21,7 +21,15 @@ def test_graphcut(parts_type):
     # blocked version should give the same result as it takes the entire image into account for each tile
     mask = pyapr.segmentation.graphcut(apr, parts, intensity_threshold=101, z_block_size=16, z_ghost_size=16)
     cc2 = pyapr.measure.connected_component(apr, mask, output=pyapr.ByteParticles())
-    assert np.all(np.array(cc2) == np.array(cc))
+    if not np.all(np.array(cc2) == np.array(cc)):
+        a = np.array(cc2)
+        b = np.array(cc)
+        diff = a != b
+        with np.printoptions(threshold=200):
+            print(np.sum(diff))
+            print(a[diff])
+            print(b[diff])
+        assert 0
 
     foreground, background = pyapr.segmentation.compute_terminal_costs(apr, parts)
 
