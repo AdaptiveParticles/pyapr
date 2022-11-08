d6388cf2e2bae7cd75c69b28954100139c80f5d4
joeljonsson <38280105+joeljonsson@users.noreply.github.com>
2022-11-08 10:04:13 +0000

    Deploying to gh-pages from @ AdaptiveParticles/pyapr@6a0bc22ac76b3296d8d3f409af2e2a89c5dc1204 🚀

---
+++ v1.0.4/_static/documentation_options.js
@@ -0,0 +1,14 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '1.0.5.dev0+g6a0bc22.d20221108',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
